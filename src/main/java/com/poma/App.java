1486845ed750e0fbd7223ab313d02bdd91f987ba
diegogr94 <diegoguerreromil994@gmail.com>
2025-04-25 20:21:09 +0200

    imagenFondo

--- src/main/java/com/poma/App.java
+++ src/main/java/com/poma/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,6 +21,7 @@ public class App extends Application {
         scene = new Scene(loadFXML("Vista1"), 640, 480);
         stage.setScene(scene);
         stage.show();
+
     }
 
     static void setRoot(String fxml) throws IOException {
