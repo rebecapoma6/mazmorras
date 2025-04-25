0fc8ce0318cbe1d43ead9fd37b258cbb363bbbc2
ronic <ralealp01@iesalbarregas.es>
2025-04-25 20:49:01 +0200

    Creacion_vista2

--- src/main/java/com/poma/SceneManager.java
+++ src/main/java/com/poma/SceneManager.java
@@ -3,6 +3,9 @@ package com.poma;
 import java.io.IOException;
 import java.util.HashMap;
 
+import com.poma.controller.Vista2Controller;
+import com.poma.model.Protagonista;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,6 +15,7 @@ public class SceneManager {
     private static SceneManager instance;
     private Stage stage;
     private HashMap<SceneId, Scene> scenes;
+    private Protagonista protagonista;
 
     private SceneManager() {
         scenes = new HashMap<>();
@@ -34,16 +38,27 @@ public class SceneManager {
             FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("views/" + fxml + ".fxml"));
             Parent root = fxmlLoader.load();
             Scene scene = new Scene(root, 600, 400);
+            scene.setUserData(fxmlLoader);//Almacena el FXMLLoader en el UserData
             scenes.put(sceneID, scene); // Almacena la escena en el mapa con el identificador correspondiente
         } catch (IOException e) {
             e.printStackTrace(); // En caso de error al cargar el FXML
         }
     }
 
-    public void loadScene(SceneId sceneID) {
+    public void loadScene(SceneId sceneID, Protagonista protagonista) {
+        this.protagonista = protagonista;
         if (scenes.containsKey(sceneID)){
             stage.setScene(scenes.get(sceneID)); // Establece la escena en la ventana principal
             stage.show(); // Muestra la ventana con la nueva escena
+        }
+
+        //Pasar el protagonista al controlador de la nueva escena
+        if(scenes.get(sceneID).getUserData() instanceof FXMLLoader){
+            FXMLLoader loader = (FXMLLoader) scenes.get(sceneID).getUserData();
+            Vista2Controller controller = loader.getController();
+            if (controller != null){//Verificar que el controlador no sea nulo
+                controller.setProtagonista(protagonista); //Pasa el protagonista al controlador
+            }
         }
     }
 
--- src/main/java/com/poma/controller/Vista1Controller.java
+++ src/main/java/com/poma/controller/Vista1Controller.java
@@ -2,6 +2,7 @@ package com.poma.controller;
 
 import com.poma.SceneId;
 import com.poma.SceneManager;
+import com.poma.model.Protagonista;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -25,8 +26,24 @@ public class Vista1Controller {
 
     @FXML
     public void initialize(){
-        btnIniciar.setOnAction(event ->{
-            SceneManager.getInstance().loadScene(SceneId.VISTA1);
+
+        //Recoger datos del jugador
+        //Para recoger los datos del jugador en la primera vista se debe declaras las variables con sus atributos
+        //luego se iguala al id que hemos puesto en el Scene Builder de cada campo de texto y se llama al metodo get.Text();
+        //En caso de que el campo sea TextField y que el dato que ingresamos sea un numero, tenemos que convertir ese texto
+        //en numero, por eso se llama a Interger.parseInt(mentemos el id del TextField con el metodo getText())
+         btnIniciar.setOnAction(event ->{
+        String nombre = txtNombre.getText();
+        int puntosVida = Integer.parseInt(txtPuntosVida.getText());
+        int defensa = Integer.parseInt(txtDefensa.getText());
+        int fuerza = Integer.parseInt(txtFuerza.getText());
+
+        //Crear protagonista
+        Protagonista protagonista = new Protagonista (nombre, defensa, fuerza, puntosVida, fuerza);
+
+        //Pasar el protagonista a la siguiente vista
+        SceneManager.getInstance().loadScene(SceneId.VISTA2, protagonista);
+
         });
     }
 
